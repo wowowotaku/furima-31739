25947b4720c610f903f55fea794067e9d4484798
wowowotaku <katsuhikoswork@gmail.com>
2020-12-23 06:28:34 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :user_name, presense: true
+  validates :last_name, presense: true
+  validates :first_name, presense: true
+  validates :last_name_kana, presense: true
+  validates :fitst_name_kana, presense: true
+  validates :birthday, presense: true
+  
 end
