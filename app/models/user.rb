ed514f11b683a6043b3cc7b31bb77dfd338841cc
wowowotaku <katsuhikoswork@gmail.com>
2020-12-23 19:31:33 +0900

    バリデーション2とnew.html.erbの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,16 @@ class User < ApplicationRecord
 
   validates :user_name, presence: true
   validates :last_name, presence: true
+            format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message:'Last name Full-width characters'} 
   validates :first_name, presence: true
+            format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message:'First name Full-width characters'} 
   validates :last_name_kana, presence: true
+            format: { with: /\A[ァ-ヶ一]+\z/, message:'Last name kana Full-width katakana characters'} 
   validates :fitst_name_kana, presence: true
+            format: { with: /\A[ァ-ヶ一]+\z/, message:'First name kana Full-width katakana characters'} 
   validates :birthday, presence: true
   
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Password Include both letters and numbers' 
+
 end
