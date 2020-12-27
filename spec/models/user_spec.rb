e4cb0072d67b43cf031725991ca6e8bd08b1a570
wowowotaku <katsuhikoswork@gmail.com>
2020-12-27 21:38:06 +0900

    一意性制約とRspec導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "ユーザー新規登録" do
+    it "ニックネームが必須であること" do
+    end
+    it "メールアドレスが必須であること" do
+    end
+    it "メールアドレスが一意性であること" do
+    end
+    it
