6c1205b805f80de6657363c75287b4b350b49c15
wowowotaku <katsuhikoswork@gmail.com>
2021-01-04 17:45:50 +0900

    with_option 修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, uniqueness: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
-  validates :password_confirmation, presence: true
-
   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
     validates :last_name
     validates :first_name
@@ -20,6 +12,19 @@ class User < ApplicationRecord
   with_options format: { with: /\A[ァ-ヶ一]+\z/ } do
     validates :last_name_kana
     validates :first_name_kana
+  end
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birthday
+    validates :password_confirmation
+    with_options uniqueness: true do
+      validates :nickname
+    end
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
