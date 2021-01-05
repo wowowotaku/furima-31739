de72341e14b9ec647785dfb9bedf935a37682c87
wowowotaku <katsuhikoswork@gmail.com>
2021-01-05 22:44:16 +0900

    モデルとマイグレーションファイルの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :item_name
+    validates :category_id
+    validates :explain
+    validates :price
+    validates :status_id
+    validates :shipping_fee_id
+    validates :from_id
+    validates :day_id
+  end
+  
+  belongs_to :user
+  has_one :buy_record
+  
 end
